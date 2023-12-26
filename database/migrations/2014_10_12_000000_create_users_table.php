efde40f4f70a8a65768e4f6622b681980a1ddbf4
Misery Turner <59616944+miseryturner@users.noreply.github.com>
2023-12-26 22:39:34 +0500

    start migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,13 +13,24 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('last_name');
+            $table->string('patronymic');
+            $table->string('mobile');
             $table->string('password');
-            $table->rememberToken();
+            $table->string('policy');
+            $table->date('birthday');
+            $table->string('gender', 1);
+            $table->string('address');
             $table->timestamps();
         });
+//        ● last_name – фамилия (строка)
+//        ● patronymic – отчество (строка)
+//        ● mobile – телефон (строка, минимум 12 цифр)
+//        ● password – пароль (строка, минимум 6 символов)
+//        ● policy – номер полиса (строка, минимум 16 цифр)
+//        ● birthday – дата рождения (дата, формат гггг-мм-дд)
+//        ● gender – пол («М» или «Ж»)
+//        ● address – адрес (строка)
     }
 
     /**
